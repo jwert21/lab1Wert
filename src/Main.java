23304c4a609ff48d81024dd0fe7365a8b3906146
jwert21 <jonahwert07@gmail.com>
2026-01-20 11:49:12 -0500

    added main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args){
-        Motorcycle ml = new Motorcycle();
+        Motorcycle ml = new Motorcycle(color1: "RED");
+        System.out.println(m1.getColor());
+
+        Motorcycle m2 = new Motorcycle(color1: "Blue");
+        System.out.println(m2.getColor());
     }
 }
