e4c921aa3b48bd8b2613d6d82e2abdfb08b2733b
Jonah <jonahwert07@gmail.com>
2026-01-22 21:09:34 -0500

    Added third motorcycle

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args){
-        Motorcycle m1 = new Motorcycle( "RED");
+        Motorcycle m1 = new Motorcycle( "Red");
         System.out.println(m1.getColor());
 
         Motorcycle m2 = new Motorcycle( "Blue");
         System.out.println(m2.getColor());
+
+        Motorcycle m3 = new Motorcycle("Purple");
+        System.out.println(m3.getColor());
 
         Engine myEng = new Engine( "650");
         System.out.println(myEng.getCC());
