581f037ffe34135a57efcf52227207277b0d9316
Jonah <jonahwert07@gmail.com>
2026-01-22 23:48:02 -0500

    Added code to Wheel class

--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -1,6 +1,23 @@
+/**
+
+ * Project: Lab 1
+ * Purpose Details: Motorcycle
+ * Course: IST 242
+ * Author: Joe Oakes
+ * Date Developed: 1/22/2026
+ * Last Date Changed: 1/23/2026
+ * Rev: 1/23/2026
+
+ */
+
+// Motorcycle class
 public class Motorcycle{
+    /**
+     * The color of the motorcycle
+     */
 
     private String colorA;
+
 
     public Motorcycle(String color1){
         this.colorA = color1;
